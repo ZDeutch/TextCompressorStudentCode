fe71a08f6ba360bc38c4a6cb90b7a53663657dd7
ZDeutch <zander.deutch@menloschool.org>
2025-12-04 23:14:38 -0800

    v1 submission

--- src/TextCompressor.java
+++ src/TextCompressor.java
@@ -21,17 +21,81 @@
  *  = 43.54% compression ratio!
  ******************************************************************************/
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
- *  The {@code TextCompressor} class provides static methods for compressing
- *  and expanding natural language through textfile input.
+ * The {@code TextCompressor} class provides static methods for compressing
+ * and expanding natural language through textfile input.
  *
- *  @author Zach Blick, Zander Deutch
+ * @author Zach Blick, Zander Deutch
  */
 public class TextCompressor {
 
     private static void compress() {
+        // Read all the input into a string and then break that down into chars
+        String input = BinaryStdIn.readString();
+        Character[] chars = new Character[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            chars[i] = input.charAt(i);
+        }
 
-        // TODO: Complete the compress() method
+        // Count how often each character appears through extended ASCII
+        int[] frequency = new int[256];
+        for (int i = 0; i < chars.length; i++) {
+            frequency[chars[i]]++;
+        }
+
+        // Find which characters are actually used to narrow down possible characters
+        ArrayList<Character> uniqueChars = new ArrayList<>();
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] > 0) {
+                uniqueChars.add((char) i);
+            }
+        }
+
+        // Sort characters by frequency through a simple bubble sort algorithm
+        // This isn't most efficient but the list is relatively small so it's fine
+        for (int i = 0; i < uniqueChars.size() - 1; i++) {
+            for (int j = 0; j < uniqueChars.size() - i - 1; j++) {
+                char first = uniqueChars.get(j);
+                char second = uniqueChars.get(j + 1);
+                if (frequency[first] < frequency[second]) {
+                    // Swap the second and first chars if the second is more frequent
+                    uniqueChars.set(j, second);
+                    uniqueChars.set(j + 1, first);
+                }
+            }
+        }
+
+        // Assign shorter codes to more frequent characters through a hashmap
+        HashMap<Character, Integer> charCode = new HashMap<>();
+        for(int i = 0; i < uniqueChars.size(); i++) {
+            charCode.put(uniqueChars.get(i), i);
+        }
+
+        // Figure out how many bits are needed for the codes
+        int numChars = uniqueChars.size();
+        int bits = 1;
+
+        // Need some way to figure out how many bits we need for codes
+        // Not sure how to do that yet
+
+        // Write how many unique characters there are
+        BinaryStdOut.write(numChars, 8);
+
+        // Then write each character in order of their code
+        for(int i = 0; i < uniqueChars.size(); i++) {
+            BinaryStdOut.write(uniqueChars.get(i), 8);
+        }
+
+        // Write how many bits per code
+        // Again I'm not sure how to do this yet
+        BinaryStdOut.write(bits, 8);
+
+        // Write original length
+        BinaryStdOut.write(chars.length);
+
 
         BinaryStdOut.close();
     }
@@ -44,7 +108,7 @@ public class TextCompressor {
     }
 
     public static void main(String[] args) {
-        if      (args[0].equals("-")) compress();
+        if (args[0].equals("-")) compress();
         else if (args[0].equals("+")) expand();
         else throw new IllegalArgumentException("Illegal command line argument");
     }
